dcd6f009b6845506d818a2f6f55409892efe6f9e
KristinaPq <109619349+KristinaPq@users.noreply.github.com>
2022-09-06 00:49:29 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+// "" + 1 + 0               ответ 10
+// "" - 1 + 0               ответ -1
+// true + false             ответ 1
+// 6 / "3"                  ответ 2
+// "2" * "3"                ответ 6
+// 4 + 5 + "px"             ответ 9px
+// "$" + 4 + 5              ответ $45
+// "4" - 2                  ответ 2
+// "4px" - 2                ответ NaN
+// "  -9  " + 5             ответ -9 5
+// "  -9  " - 5             ответ -14
+// null + 1                 ответ 1
+// undefined + 1            ответ NaN
+// " \t \n" - 2             ответ -2
+// null || 2 || undefined   ответ 2
+// alert(1) || 2 || alert(3)ответ 2
+// alert( 1 && null && 2 )  ответ undefined
+// alert(1) && alert(2)     ответ undefined
+// null || 2 && 3 || 4      ответ 3
+
+
+// тест последний
+// https://docs.google.com/forms/d/1Ypg6SqdMTsktmbQ89cWOxgrvbv1jISsCVJggxJ5sgPE/viewform?edit_requested=true
